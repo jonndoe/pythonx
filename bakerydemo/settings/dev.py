c81f4877a65e26c5b46d7d7d1e770a6436fac714
jonndoe <qb89@yandex.ru>
2020-06-21 23:52:43 +0300

    added mail backend

--- bakerydemo/settings/dev.py
+++ bakerydemo/settings/dev.py
@@ -1,8 +1,20 @@
 from .base import *  # noqa: F403, F401
+import os
 
 DEBUG = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp.yandex.ru'
+#EMAIL_HOST_USER = 'pythonix@yandex.ru'
+#EMAIL_HOST_PASSWORD = 'password'
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#SERVER_EMAIL = EMAIL_HOST_USER
+#DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 # BASE_URL required for notification emails
 BASE_URL = 'http://localhost:8000'
--- bakerydemo/settings/production.py
+++ bakerydemo/settings/production.py
@@ -125,3 +125,14 @@ LOGGING = {
         },
     },
 }
+
+
+# for real mail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_HOST_USER = 'pythonix@yandex.ru'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
