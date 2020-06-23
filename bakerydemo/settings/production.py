decc6b451a11be49c0ff7e6d9c8f65c12ef3fa9a
jonndoe <qb89@yandex.ru>
2020-06-23 18:16:18 +0300

    configuring email registration

--- bakerydemo/settings/production.py
+++ bakerydemo/settings/production.py
@@ -130,7 +130,7 @@ LOGGING = {
 # for real mail
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_HOST_USER = 'pythonix@yandex.ru'
+EMAIL_HOST_USER = 'registration@pythonx.ru'
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
