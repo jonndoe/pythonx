a621143244c3491f6f1ac9cd8125ba1ffb1e6559
jonndoe <qb89@yandex.ru>
2020-07-01 02:28:57 +0300

    ch24

--- bakerydemo/settings/production.py
+++ bakerydemo/settings/production.py
@@ -130,7 +130,8 @@ LOGGING = {
 # for real mail
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
+#EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_USER = 'qb89@yandex.ru'
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
